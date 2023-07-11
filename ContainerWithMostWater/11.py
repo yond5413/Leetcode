bd04a2082bcc4a2ba26b50f8ff2aa5ae1503b2b4
yond5413 <87887705+yond5413@users.noreply.github.com>
2023-07-11 15:43:11 -0400

    COntainer with most water done
    Cool Problem I really like the two pointer approach

---
+++ ContainerWithMostWater/11.py
@@ -0,0 +1,44 @@
+'''
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+ 
+
+Constraints:
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
+'''
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        ret = 0
+        l,r = 0, len(height)-1
+        #####################
+        # based on problem the minnimum height of lines is the limiting factor/tie breaker for the problem
+        ## two pointer approach helps us find max area quite quickly in this instance and work our way down 
+        while(l!=r):
+            area = (r-l)*min(height[l],height[r])
+            ret = max(ret,area)
+            if height[r] == min(height[l],height[r]):
+                r-=1
+            else:
+                l+=1
+        return ret
