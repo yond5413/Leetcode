314ee38a5b4a356d60edc990972ac3ad60da978e
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-06 13:22:32 -0500

    Time: 38 ms (89.46%) | Memory: 19.2 MB (10.35%) - LeetSync

--- 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.py
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.py
@@ -11,15 +11,11 @@ class Solution:
         if not root:
             return 0
         def dfs(curr,tot):
-            print('hi')
             if not curr or not curr.children:
-                print(not curr or curr.children)
                 return tot
             ret = 0
-            print('hi')
             for child in curr.children:
                 if child:
                     ret = max(ret,dfs(child,tot+1))
-                print(ret)
             return ret
         return dfs(root,1)
