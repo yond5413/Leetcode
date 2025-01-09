6fc16578ea7e4f3f11c1772d7a547720a7b9c4ad
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-09 17:43:52 -0500

    Time: 126 ms (82.63%) | Memory: 32.3 MB (14.07%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,6 +6,10 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        '''
+        basically as we go across each path we are keeping track of the amount of 
+        times across each path the max increases
+        '''
         def dfs(node,maximum):
             if not node:
                 return 0
