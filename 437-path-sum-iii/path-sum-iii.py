540eca89e7b258b85aedd94f5f43253f0c08b59c
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-13 14:21:07 -0500

    Time: 7 ms (66.70%) | Memory: 18.3 MB (28.83%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -10,12 +10,13 @@ class Solution:
         lookup = defaultdict(int)
         lookup[0] = 1
         def dfs(curr,total):
-            count = 0
+            ret = 0
             if curr:
-                total += curr.val
-                count += lookup[total-targetSum] 
+                total+=curr.val
+                ret += lookup[total-targetSum]
                 lookup[total]+=1
-                count += dfs(curr.left,total) + dfs(curr.right,total)
+                ret+= dfs(curr.left,total)+dfs(curr.right,total)
                 lookup[total]-=1
-            return count
+
+            return ret
         return dfs(root,0)
