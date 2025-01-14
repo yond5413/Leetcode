4b9a75e6517573d1fb0fdcd20b54073876c77fb3
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-14 07:03:56 -0500

    Time: 3 ms (8.05%) | Memory: 18 MB (13.57%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         ret = []
-        adj= defaultdict(dict)
+        adj = defaultdict(dict)
         for i in range(len(equations)):
             u,v = equations[i]
             adj[u][v] = values[i]
@@ -13,16 +13,13 @@ class Solution:
             if target in adj[root]:
                 return adj[root][target]
             visited.add(root)
-            ##########################
             for vert in adj[root]:
                 cost = adj[root][vert]
                 if vert not in visited:
                     val = dfs(vert,target,visited)
                     if val !=-1:
-                        return val*cost
-            return -1 
-        for i in range(len(queries)):
-            u,v = queries[i]
+                        return cost*val
+            return -1
+        for u,v in queries:
             ret.append(dfs(u,v,set()))
-        ############################
         return ret
