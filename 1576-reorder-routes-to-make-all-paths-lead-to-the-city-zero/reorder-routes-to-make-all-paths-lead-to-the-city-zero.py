f4442cc7080dd880fef1f95256a7c2ebe22b983c
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-14 05:53:33 -0500

    Time: 193 ms (56.48%) | Memory: 50 MB (52.29%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,16 +1,16 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         '''
-        setup adj_list
-        dfs 
-        tada
+        - adj_list: maintains next node and direction
+        - flip connections are 1 ie postive (exiting from 0)
         '''
         adj_list = [[] for _ in range(n)]
+        #visited = set()
         for i in range(n-1):
-            u,v = connections[i][0], connections[i][1]
+            u,v = connections[i][0],connections[i][1]
             adj_list[u].append((v,1))
             adj_list[v].append((u,-1))
-        #visited = set()
+        ####################################
         def dfs(adj,visited,curr_vert):
             total = 0
             if curr_vert not in visited:
@@ -18,8 +18,8 @@ class Solution:
             for pair in adj[curr_vert]:
                 vert,direction = pair
                 if vert not in visited:
-                    if direction !=-1:
-                        total+=1
+                    if direction == 1:
+                        total +=1
                     total += dfs(adj,visited,vert)
             return total
         return dfs(adj_list,set(),0)
