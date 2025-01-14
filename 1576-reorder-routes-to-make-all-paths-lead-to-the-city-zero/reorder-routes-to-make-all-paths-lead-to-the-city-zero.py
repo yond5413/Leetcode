32087310b6c846c8f55acf13990564d66fde5f20
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-14 06:13:58 -0500

    Time: 180 ms (70.76%) | Memory: 49.8 MB (52.56%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -14,14 +14,12 @@ class Solution:
         def dfs(adj,visited,curr_vert):
             total = 0
             if curr_vert not in visited:
-                visited.add(curr_vert)
-            else:
-                return total
-            for pair in adj[curr_vert]:
-                vert,direction = pair
-                if vert not in visited:
-                    if direction == 1:
-                        total +=1
-                    total += dfs(adj,visited,vert)
+                visited.add(curr_vert)    
+                for pair in adj[curr_vert]:
+                    vert,direction = pair
+                    if vert not in visited:
+                        if direction == 1:
+                            total +=1
+                        total += dfs(adj,visited,vert)
             return total
         return dfs(adj_list,set(),0)
