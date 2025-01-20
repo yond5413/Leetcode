47ace4279fe355a610c5e22d1a96223fa6b216f6
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-20 07:33:15 -0500

    Time: 40 ms (40.76%) | Memory: 101.6 MB (81.41%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,33 +1,25 @@
-#include  <algorithm>
-
+#include <algorithm>
+#include <iostream>
+#include <vector>
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        // Assuming potions are sorted rn?
-        vector<int> ret(spells.size());
-        int n = spells.size(),  m = potions.size();
-        /*
-        Basically going to multiply
-        */
+        int n = spells.size(), m = potions.size();
+        vector<int> ret(n);
         sort(potions.begin(),potions.end());
-        for (int i = 0; i<n; i++){
-            long long int power = spells[i];
-                    int l = 0, r = m-1;
-                    int mid;
-                    while(l<=r){
-                        mid = l+(r-l)/2;
-                        long long prod = power*(long long int)potions[mid];
-                        //cout << "i:"<< i<<" Prod: "<<prod<<"\n";                       
-                        if( prod>= success){
-                            r = mid - 1;
-                        }
-                        else{
-                            l = mid + 1;
-                        }
-                    }
-                    //ret.push_back(m-l);/////??????
-                ret[i] = m-l;
+        for(int i = 0; i<n;i++){
+            int l = 0, r = m-1;
+            while(l<=r){
+                int mid = l+(r-l)/2;
+                if (static_cast<long long>(spells[i]*static_cast<long long>(potions[mid]))>=success){
+                    r = mid-1;
+                }
+                else{
+                    l = mid+1;
+                }
             }
-            return ret;
+            ret[i] = m-l;
+        }
+        return ret;
     }
 };
