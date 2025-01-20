631206838575f35982a5940b0992854795072bfa
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-20 08:10:22 -0500

    Time: 43 ms (28.20%) | Memory: 101.8 MB (61.28%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,22 +1,22 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         int n = spells.size(), m = potions.size();
         vector<int> ret(n);
         sort(potions.begin(),potions.end());
-        for(int i = 0; i<n;i++){
+        for (int i = 0; i<n;i++){
             int l = 0, r = m-1;
             while(l<=r){
-                int mid = l+(r-l)/2;
-                if (static_cast<long long>(spells[i]*static_cast<long long>(potions[mid]))>=success){
+             int mid = l+(r-l)/2;
+                if ((static_cast<long long>(spells[i])*static_cast<long long>(potions[mid]))>=success){
                     r = mid-1;
-                }
+                 }
                 else{
                     l = mid+1;
-                }
+             }  
             }
             ret[i] = m-l;
         }
