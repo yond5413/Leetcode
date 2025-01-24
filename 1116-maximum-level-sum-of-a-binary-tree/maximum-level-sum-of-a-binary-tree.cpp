921fe6e9487c34b96f3a5d27f0468be36a2e5f73
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-24 14:05:38 -0500

    Time: 4 ms (57.77%) | Memory: 109.4 MB (56.79%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -11,26 +11,22 @@
  */
 #include <queue>
 #include <cmath>
-
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
         int ret = 0;
-        if (root == nullptr){
-            return ret;      
-        }
+        int level = 1;
+        int curr_max = -pow(10,5);
         queue<TreeNode*> q;
         q.push(root);
-        int curr_max = pow(-10,5); //pow(base,exp) 
-        int level = 1;
         while(!q.empty()){
-            int tot = 0;
-           int n = q.size();
-            for( int i = 0; i<n;i++){
+            int tot  =0;
+            int n = q.size();
+            for(int i = 0; i<n; i++){
                 TreeNode* curr = q.front();
                 q.pop();
                 if (curr != nullptr){
-                    tot+= curr->val;
+                    tot +=curr->val;
                     if(curr->left != nullptr){
                         q.push(curr->left);
                     }
@@ -38,15 +34,15 @@ public:
                         q.push(curr->right);
                     }
                 }
-                //std:: cout<< "curr->val: " << curr->val<< " tot: "<< tot<<"\n";
             }
-            //////////////////////////////////
-            if (curr_max < tot){
+            if (curr_max<tot){
                 ret = level;
                 curr_max = tot;
             }
-            level+=1;
+            level++;
+
         }
-    return ret;
+
+        return ret;
     }
 };
