4e5fc6644c32e7d37987a79df4ebea51b1aa809a
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-26 10:14:22 -0500

    Time: 23 ms (30.84%) | Memory: 64.2 MB (41.09%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.cpp
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -1,32 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include<deque>
-/////////////////////////////
 class RecentCounter {
 public:
     queue<int> q;
-
     RecentCounter() {
-        std:: cout<< "lmao \n";
+        
     }
     
     int ping(int t) {
-     /*
-      1<= t <= 10**4
-        return requests in inclusive range:
-        [t-3000,t]     
-      */
-        int lower =  t-3000;
-        //vector<int> q;
+        int lower =t-3000;
         q.push(t);
         while(!q.empty() && q.front()<lower){
             q.pop();
         }
         return q.size();
-
     }
-
 };
 
 /**
