11214cb75cc08315003de81a6ec61a4a1fd01ebd
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-27 10:47:51 -0500

    Time: 3 ms (64.55%) | Memory: 11.5 MB (60.37%) - LeetSync

--- 649-dota2-senate/dota2-senate.cpp
+++ 649-dota2-senate/dota2-senate.cpp
@@ -1,32 +1,32 @@
+#include <iostream>
+#include <queue>
 class Solution {
 public:
     string predictPartyVictory(string senate) {
         queue<int> r_queue;
-        queue<int>d_queue;
+        queue<int> d_queue;
         int n = senate.size();
-        ///////////////////////
-        for(int i = 0; i<n;i++){
-            if (senate[i] =='R'){
+        for(int i = 0;i<n;i++){
+            if (senate[i] == 'R'){
                 r_queue.push(i);
             }
             else{
                 d_queue.push(i);
             }
         }
-        ///////////////////////
-        while(!d_queue.empty() && !r_queue.empty()){
-            int r =r_queue.front();
-            r_queue.pop();
+        while(!r_queue.empty() && !d_queue.empty()){
+            int r = r_queue.front();
             int d = d_queue.front();
             d_queue.pop();
-            if (d<r){
-                d_queue.push(d+n);
-            }
-            else{
+            r_queue.pop();
+            ///////////////////
+            if (r<d){
                 r_queue.push(r+n);
             }
+            else{
+                d_queue.push(d+n);
+            }
         }
-        return d_queue.empty() ? "Radiant": "Dire";
-    //
+        return d_queue.empty() ? "Radiant" :"Dire";
     }
 };
