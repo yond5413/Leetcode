4e33a39871b7175988878e9f04ff5b1c6b8a2880
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-02-04 15:46:09 -0500

    Time: 0 ms (100.00%) | Memory: 34.4 MB (28.70%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -15,32 +15,28 @@ public:
         if (root == nullptr){
             return root;
         }
-        /////////////////
-        if (root->val <key){
+        if (root->val < key){
             root->right = deleteNode(root->right,key);
         }
         else if (root->val > key){
             root->left = deleteNode(root->left,key);
         }
-        /////////////////
         else{
-            if (root->left != nullptr && root->right != nullptr){
-            TreeNode* curr = root->right;
-            while( curr->left!= nullptr){
-                curr = curr->left;
-            }
-            root->val = curr->val;
-            root->right = deleteNode(root->right,root->val);
-        } 
-            else if (root->left == nullptr){
+            if (root->left == nullptr){
                 return root->right;
             }
-            else{
+            else if (root->right == nullptr){
                 return root->left;
             }
+            else{
+               TreeNode* curr = root->right;
+                while(curr->left != nullptr){
+                    curr = curr->left;
+                }
+                root->val = curr->val;
+                root->right = deleteNode(root->right,root->val);
+            }
         }
-
-
         return root;
     }
 };
