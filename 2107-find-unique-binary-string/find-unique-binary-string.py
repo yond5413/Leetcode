2b0721be2b9b68beed34151249afc09a914baebe
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-02-20 12:42:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (52.45%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -2,20 +2,21 @@ class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         n = len(nums)
         lookup = []
-        visited =set() #set(nums)
+        visited =set(nums)
         #print(visited)
         def dfs(curr: str):
             if len(curr) == n and curr not in visited:
                 lookup.append(curr)
                 visited.add(curr)
-                return 
+                return lookup[-1]
             for i in ['0','1']:
                 if (curr+i) not in visited:
                     #visited.add(curr+i)
                     dfs(curr+i) 
+                    if lookup:
+                        return lookup[-1]
                     #visited.remove(curr+i)
+            return 
         dfs('')
-        for i in range(len(lookup)):
-            if lookup[i] not in nums:
-                return lookup[i]
-        return ''
+        return '' if not lookup else lookup[0]
+        
