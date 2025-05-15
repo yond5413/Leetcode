9d60d56359edf63766f440246605e51b8d157cd6
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-05-15 17:44:17 -0400

    Time: 269 ms (32.80%) | Memory: 25.5 MB (21.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,20 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n = len(grid),len(grid[0])
-        visited = [[False for _ in range(n)] for _ in range(m)]
         ret = 0
+        visited = set()
         for i in range(m):
             for j in range(n):
-                if not visited[i][j] and grid[i][j] =='1':
+                if grid[i][j] =='1' and (i,j) not in visited:
                     ret+=1
-                    queue = [(i,j)]
-                    visited[i][j] = True
-                    while(queue):
-                        curr_x,curr_y = queue.pop(0)
-                        adj_list = [(curr_x+1,curr_y),(curr_x-1,curr_y),(curr_x,curr_y+1),(curr_x,curr_y-1)]
+                    visited.add((i,j))
+                    stack = [(i,j)]
+                    while(stack):
+                        curr_x,curr_y = stack.pop()
+                        adj_list = [(0,1),(1,0),(0,-1),(-1,0)]
                         for x,y in adj_list:
-                            if x>=0 and x<m and y>=0 and y<n:
-                                if not visited[x][y] and grid[x][y] == '1':
-                                    queue.append((x,y))
-                                    visited[x][y] = True
+                            a = curr_x+x
+                            b = curr_y+y
+                            if (a,b) in visited:
+                                continue
+                            elif (a)>=0 and (a)<m and (b)>=0 and (b)<n and grid[a][b] == '1':
+                                visited.add((a,b))
+                                stack.append((a,b))
+
         return ret
