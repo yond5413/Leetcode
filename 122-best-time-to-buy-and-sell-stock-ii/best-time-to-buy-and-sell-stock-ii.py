1b7d5c27d3f164ccc243cf67629cba653694b319
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-06-01 16:49:09 -0400

    Time: 35 ms (5.01%) | Memory: 27.7 MB (5.49%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+        def dfs(i,buy):
+            if i==len(prices):
+                return 0
+            if (i,buy) in dp:
+                return dp[(i,buy)]
+            ##############################
+            hold = dfs(i+1,buy)
+            if buy:
+                profit = dfs(i+1,not buy)-prices[i]
+            else:
+                profit = dfs(i+1,not buy)+prices[i]
+            dp[(i,buy)] = max(profit,hold)
+            return dp[(i,buy)]
+           
+        
+        return dfs(0,True) 
