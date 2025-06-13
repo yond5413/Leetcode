d9a82a19de407abfcb38090e57d73b3e8a4c0e44
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-06-13 16:42:55 -0400

    Time: 7 ms (47.14%) | Memory: 20.8 MB (82.87%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,14 +6,17 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        ret = 0
+        ret = 0 # stores diameter
+        ## dfs is too find hiehgt
         def dfs(curr):
             if not curr:
                 return 0
             nonlocal ret
-            left =  dfs(curr.left)
+            left = dfs(curr.left)
             right = dfs(curr.right)
-            ret = max(ret,left+right)
-            return max(left,right)+1
+            ret = max(ret,left +right) # diameter update 
+            # difference is that this takes into account going upda and down tree
+            ## takes into account prior subproblems
+            return max(left,right)+1 # max (height) update
         dfs(root)
         return ret
