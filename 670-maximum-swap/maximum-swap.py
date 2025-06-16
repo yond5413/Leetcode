54e39fbcbb377f2db6045c545eeae718d3de8d21
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-06-16 14:44:32 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (35.26%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,22 +1,15 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        '''
-        Ideally skip 9's
-        1 swap to get max num
-        '''
-        str_num = list(str(num))
-        n = len(str_num)
+        num_s = list(str(num))
+        n = len(num_s)
         max_r = [0]*n
-        max_r[n-1] = n-1 ## stores idx
-        ######################
-        ## 2 passes to compute
-        # first -> find index of largest value from right
+        max_r[n-1] = n-1
+        # 1st Pass
         for i in range(n-2,-1,-1):
-            max_r[i] = i if int(str_num[i])>int(str_num[max_r[i+1]]) else max_r[i+1]
-        # second
+            max_r[i] = i if int(num_s[i]) > int(num_s[max_r[i+1]]) else max_r[i+1]
+        # 2nd Pass
         for i in range(n):
-            if int(str_num[i]) < int(str_num[max_r[i]]):
-                str_num[i],str_num[max_r[i]]=str_num[max_r[i]], str_num[i]  
-                return int(''.join(str_num))
-
-        return num # no swap
+            if int(num_s[i])< int(num_s[max_r[i]]):
+                num_s[i],num_s[max_r[i]] = num_s[max_r[i]],num_s[i]
+                return int(''.join(num_s))
+        return num
