d007e134e9391d1084002e451721042141a1b21c
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-06-22 13:32:15 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.14%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -4,25 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import defaultdict
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
         queue = [(root,0)]
-        lookup = {}
+        lookup = defaultdict(list)
         l,r = 0,0
         while queue:
             curr,level = queue.pop(0)
-            if level not in lookup:
-                lookup[level] = []
             lookup[level].append(curr.val)
             if curr.left:
                 queue.append((curr.left,level-1))
-                l = min(l,level-1)
+                l = min(level-1,l)
             if curr.right:
                 queue.append((curr.right,level+1))
-                r = max(r,level+1)
-            
+                r = max(level+1,r)
         ret = []
         for i in range(l,r+1):
             ret.append(lookup[i])
