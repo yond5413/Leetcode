f6cad2a6952d96a8d69c9b0dfc77b48a508c6d7e
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-06-24 12:48:00 -0400

    Time: 5 ms (0.25%) | Memory: 17.8 MB (38.88%) - LeetSync

---
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -0,0 +1,29 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        # can replace substring with length of it
+        # no leading zeroes in abbr
+        # cannot be adjacent as it can be confusing
+        i,j = 0,0
+        m,n = len(abbr),len(word)
+        while(j<m and i<n):
+            print(f"i:{i},j:{j}")
+            if abbr[j].isdigit():
+                offset = 0
+                if abbr[j] =='0':
+                    return False
+                while(j<m and abbr[j].isdigit()):
+                    offset= offset*10+int(abbr[j])
+                    j+=1
+                if i+offset<=n:
+                    i+=offset
+                else:
+                    #print('TRUST')
+                    return False
+            elif abbr[j]!=word[i]:
+                #print('LMAO')
+                return False
+            else:
+                i+=1
+                j+=1
+        return True if i ==n and j == m else False
+        
