a8d30fd60368f07e2350adf025d809f06d7a6bcd
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-06-29 11:24:02 -0400

    Time: 11 ms (30.04%) | Memory: 20.9 MB (62.86%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,7 +3,7 @@ class Solution:
         intervals.sort(key =lambda x: x[0])
         ret = []
         curr = intervals[0]
-        flag = True
+        
         for i in range(1,len(intervals)):
             if curr[1]>=intervals[i][0]:
                 curr[1] = intervals[i][1] if intervals[i][1]>curr[1] else curr[1]
