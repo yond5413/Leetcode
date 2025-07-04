940cebce059d3ac6b70c016c4e259955e16cc7ce
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-07-04 05:07:50 -0400

    Time: 103 ms (21.68%) | Memory: 18.8 MB (98.52%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,32 +1,32 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        ## MDAS
-        ret=prev=curr = 0
-        curr_opp = '+'
-        i=0
-        while(i<len(s)):
-            curr_char = s[i]
+        prev= curr = ret = 0
+        i = 0
+        curr_op = '+'
+        while i<len(s):
+            curr_char  =s[i]
             if curr_char.isdigit():
                 while i<len(s) and s[i].isdigit():
                     curr = curr*10 + int(s[i])
                     i+=1
                 i-=1
-                if curr_opp == '+':
+                if curr_op =='+':
                     ret +=curr
                     prev = curr
-                elif curr_opp == '-':
-                    ret -= curr
+                elif curr_op =='-':
+                    ret -=curr
                     prev = -curr
-                elif curr_opp == '*':
-                    ret-=prev
+                elif curr_op =='*':
+                    ret -=prev
                     ret += prev*curr
-                    prev = prev*curr
-                elif curr_opp == '/':
-                    ret -= prev
-                    ret+= int(prev/curr)#prev//curr
-                    prev = int(prev/curr)#prev//curr
+                    prev =  prev*curr
+                elif curr_op =='/':
+                    ret -=prev
+                    ret += int(prev/curr)
+                    prev = int(prev/curr)
                 curr = 0
             elif curr_char != ' ':
-                curr_opp = curr_char
+                curr_op = s[i]
             i+=1
+
         return ret
