1b5b631432c15a09c8edc7f6b2933b9d22fd406c
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-07-18 15:41:57 -0400

    Time: 1392 ms (20.24%) | Memory: 44.4 MB (28.38%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,46 +1,40 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         lookup = {}
-        n = len(grid)
-        visited = set()
         label = 2
-        adj = [(1,0),(0,1),(-1,0),(0,-1)]
+        visited = set()
+        n  =len(grid)
+        adj = [(0,1),(1,0),(0,-1),(-1,0)]
         def is_valid(r,c):
             return r>=0 and r<n and c>=0 and c<n
-        #####################################
         for i in range(n):
             for j in range(n):
-                if grid[i][j] == 1 and (i,j) not in visited:
+                if (i,j) not in visited and grid[i][j] ==1:
                     queue = [(i,j)]
                     count = 0
-                    #visited.add((i,j))
-                    while (queue):
-                        x,y = queue.pop()
+                    visited.add((i,j))
+                    while queue:
+                        x,y = queue.pop(0)
                         grid[x][y] = label
                         for a,b in adj:
-                            if is_valid(x+a,y+b) and (x+a,y+b) not in visited and grid[x+a][y+b]==1:
+                            if (x+a,y+b) not in visited and is_valid(x+a,y+b) and grid[x+a][y+b] ==1:
                                 queue.append((x+a,y+b))
                                 visited.add((x+a,y+b))
-                        count+=1
+
+                        count +=1
                     lookup[label] = count
                     label+=1
-        print(lookup)
         def connect(r,c):
-            ret = 1
-            new_visited = set()
-            for i,j in adj:
-                if is_valid(r+i,c+j) and grid[r+i][c+j] not in new_visited and grid[r+i][j+c] in lookup:
-                    ret+=lookup[grid[r+i][c+j]]
-                    new_visited.add(grid[r+i][c+j])
+            ret =1
+            new_set = set()
+            for x,y in adj:
+                if is_valid(x+r,y+c) and grid[x+r][y+c] not in new_set and grid[x+r][y+c]>1:
+                    ret +=lookup[grid[x+r][y+c]]
+                    new_set.add(grid[x+r][y+c])
             return ret
-
         ret = 0 if not lookup else max(lookup.values())
         for i in range(n):
             for j in range(n):
-                if grid[i][j] == 0:
+                if grid[i][j]==0:
                     ret = max(ret,connect(i,j))
         return ret
-            
-
-
-
