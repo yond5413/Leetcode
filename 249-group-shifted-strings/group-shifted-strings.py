7c66b7520e7c528b2c42d2afd446c13fb903efa9
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-07-18 21:47:46 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (43.28%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.py
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -3,13 +3,12 @@ class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         lookup = defaultdict(list)
         for s in strings:
-            if len(s) ==1:
+            if len(s) == 1:
                 lookup[-1].append(s)
             else:
-                ## the fun part
                 char_store = []
                 i = 1
-                while i <len(s):
+                while i<len(s):
                     char_store.append((ord(s[i])-ord(s[i-1]))%26)
                     i+=1
                 seq = tuple(char_store)
