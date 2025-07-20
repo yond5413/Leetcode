b55659002ec8deab16757354678c4d82d2636024
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-07-20 12:30:30 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (24.04%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.py
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -1,21 +1,13 @@
 from collections import defaultdict
 class Solution:
-    ##FOr tracking the sequences/making key for hashmap 
-    ## Either diff array like we have
-    ## OR we can normalize around a
-    ### Basically make first char of str a and offset others likewise
-    #### offset will be between 0-25 
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         lookup = defaultdict(list)
         for s in strings:
-            if len(s) == 1:
+            if len(s) <=1:
                 lookup[-1].append(s)
             else:
-                char_store = []
-                i = 1
-                while i<len(s):
-                    char_store.append((ord(s[i])-ord(s[i-1]))%26)
-                    i+=1
-                seq = tuple(char_store)
-                lookup[seq].append(s)
+                diff_array = []
+                for i in range(1,len(s)):
+                    diff_array.append((ord(s[i])-ord(s[i-1]))%26)
+                lookup[tuple(diff_array)].append(s)
         return list(lookup.values())
