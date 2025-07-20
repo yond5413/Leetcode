a5642674e5f620639a8b77c33e536b3ef7d569a1
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-07-20 13:42:39 -0400

    Time: 9 ms (18.08%) | Memory: 19.5 MB (30.96%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,12 +1,10 @@
 from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-       ### top sort 
-        adj_list = defaultdict(list)
         cache = {}
-        for course,prereq in prerequisites:
-            adj_list[prereq].append( course)
-        ############################## 
+        adj_list = defaultdict(list)
+        for c,p in prerequisites:
+            adj_list[p].append(c)
         def dfs(vert,visited):
             if vert in visited:
                 return False
@@ -16,9 +14,9 @@ class Solution:
             for c in adj_list[vert]:
                 if not dfs(c,visited):
                     cache[c] = False
-                    return False    
-            cache[vert] = True
+                    return False
             visited.remove(vert)
+            cache[vert] = True
             return True
         for i in range(numCourses):
             if not dfs(i,set()):
