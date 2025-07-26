42d0c7ab689979f49f493f2d1d258ace546a5c9b
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-07-26 11:37:56 -0400

    Time: 103 ms (5.93%) | Memory: 20.4 MB (16.45%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -4,16 +4,14 @@ class Solution:
         ret = 0
         flips = 0
         while(r<len(nums)):
-            #print(f"flips:{flips},l:{l},r:{r},nums:{nums[l:r+1]},ret:{ret}")
-            if nums[r] == 0:
-                flips +=1  
-            if  flips>k:
-                ret = max(ret,r-l)
+            if nums[r] ==0:
+                flips+=1
+            if flips >k:
                 while l<=r and flips>k:
                     if nums[l] == 0:
                         flips-=1
                     l+=1
-                
+            ret = max(ret,r-l+1)
             r+=1
-        ret = max(ret,r-l)
+            
         return ret
