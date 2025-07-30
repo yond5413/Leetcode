33be14b6821a2052aa9a78ea25386fe20d9f48b2
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-07-30 17:07:40 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (64.18%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -1,24 +1,21 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        exp_flag = False
-        deci_flag = False
-        seen_digit = False
+        digit = dot = exp = False
         for i in range(len(s)):
-            if (s[i] in ['+','-']):
+            if s[i] in ['+','-']:
                 if i>0 and s[i-1] not in ['e','E']:
                     return False
-            ###############
             elif s[i].isnumeric():
-                seen_digit = True
-            elif s[i] =='.':
-                if exp_flag or deci_flag:
+                digit = True
+            elif s[i] in ['e','E']:
+                if not digit or exp:
                     return False
-                deci_flag = True
-            elif s[i] in ['e',"E"]:
-                if exp_flag or not seen_digit:
+                digit = False
+                exp = True
+            elif s[i] == '.':
+                if dot or exp:
                     return False
-                seen_digit = False
-                exp_flag = True
+                dot = True
             else:
                 return False
-        return seen_digit
+        return digit
