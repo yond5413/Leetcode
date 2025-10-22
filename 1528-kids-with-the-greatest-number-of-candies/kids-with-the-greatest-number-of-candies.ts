b628606888abd020eb17b930904ee55d6c31c915
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-10-22 15:52:56 -0400

    Time: 0 ms (100.00%) | Memory: 55.5 MB (84.41%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
@@ -1,7 +1,7 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
     let n = candies.length 
     let ret = Array(n).fill(false);
-    let max = Math.max(...candies)
+    let max = Math.max(...candies)// ... is spread operator?
     for (let i = 0;i<n;i++){
         if (candies[i]+extraCandies >= max){
             ret[i] = true
