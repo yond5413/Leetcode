aa46e9aa02be76e9ec28781caa6335151e098ec5
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-10-28 18:50:29 -0400

    Time: 14 ms (29.12%) | Memory: 71.7 MB (43.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.ts
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,14 +1,14 @@
 function productExceptSelf(nums: number[]): number[] {
     let n = nums.length
-    let pre =  Array(n).fill(1);
+    let pre = Array(n).fill(1)
     for (let i = 1;i<n;i++){
         pre[i] = pre[i-1]*nums[i-1]
     }
-    let post = 1;
     let ret = Array(n).fill(0)
-    for (let i = n-1; i>=0;i--){
-        ret[i] = post*pre[i]
-        post*=nums[i]
+    let post = 1
+    for (let i=n-1;i>=0;i--){
+        ret[i] = pre[i]*post
+        post *=nums[i] 
     }
-    return ret;  
+    return ret;
 };
