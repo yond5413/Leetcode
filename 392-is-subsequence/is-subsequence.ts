51416ff7cefa5346f90cab90ae63b1efcf6c92f5
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-11-14 14:00:52 -0500

    Time: 1 ms (71.04%) | Memory: 54.1 MB (98.05%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.ts
@@ -0,0 +1,15 @@
+function isSubsequence(s: string, t: string): boolean {
+    if (s.length > t.length){
+        return false;
+    }
+    let p1 = 0
+    let p2 = 0
+    while (p1<s.length && p2<t.length){
+        if (s[p1]==t[p2]){
+            p1++
+        
+        }
+        p2++;
+    }
+    return p1 == s.length
+};
