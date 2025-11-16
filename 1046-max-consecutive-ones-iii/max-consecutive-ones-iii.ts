cd751c0f479e6f082040d13d883d67715784d27b
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-11-16 09:05:54 -0500

    Time: 4 ms (34.40%) | Memory: 63.1 MB (21.42%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.ts
@@ -0,0 +1,21 @@
+function longestOnes(nums: number[], k: number): number {
+    let ret = 0;
+    let l = 0;
+    let r = 0;
+    let flips = 0;
+    while (r<nums.length){
+        if (nums[r] != 1){
+            flips++;
+        }
+        while (flips>k){
+            if (nums[l] !=1){
+                flips--;
+            }
+            l++
+        }
+        ret = Math.max(ret,r-l+1)
+        r++;
+    }
+
+    return ret
+};
