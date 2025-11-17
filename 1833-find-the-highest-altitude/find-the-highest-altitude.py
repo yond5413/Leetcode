a34dba147aef743fb5dffdc3477877fd833fc96b
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-11-17 15:13:39 -0500

    Time: 3 ms (8.17%) | Memory: 18 MB (10.45%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -1,10 +1,7 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        ret = 0
         n = len(gain)
-        prefix = [0]*(n+1)
-        for i in range(1,n+1):
-            prefix[i] = gain[i-1]+prefix[i-1]
-        print(prefix)
-        ret = max(prefix)
-        return ret
+        amps = [0]*(n+1)
+        for i in range(1,len(amps)):
+            amps[i] = gain[i-1]+amps[i-1]
+        return max(amps)
