6e9b5940760810fb6ed4b59a7a836750ffa780fa
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-11-24 15:03:50 -0500

    Time: 23 ms (69.02%) | Memory: 21.9 MB (83.63%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -3,11 +3,11 @@ class Solution:
         ret = 0
         n = len(grid)
         rows = {}
+        for i in range(n):
+            curr = tuple(grid[i]) ## Lists are not hashable
+            rows[curr] = 1+rows.get(curr,0)
+        for i in range(n):
+            cols = [grid[j][i] for j in range(n)]
+            ret += rows.get(tuple(cols),0)
 
-        for i in range(n):
-            curr_row = tuple(grid[i])
-            rows[curr_row] = 1 + rows.get(curr_row,0)
-        for i in range(n):
-            col = [grid[j][i] for j in range(n)]
-            ret += rows.get(tuple(col),0)
         return ret
