7baa70fc78d5019c7e233b15b65d65242a9710a8
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-11-26 10:23:14 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (11.97%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,18 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
+        ret = ''
         digit = 0
-        ret = ""
+        stack = []
         for ch in s:
             if ch.isdigit():
                 digit = digit*10 + int(ch)
-            elif ch =="[":
+            elif ch == "[":
                 stack.append((digit,ret))
-                digit = 0
                 ret = ""
-            elif ch =="]":
+                digit = 0
+            elif ch == "]":
                 old_num,old_str = stack.pop(-1)
                 ret = old_str + ret*old_num
+                '''
+                Essentially old_str is done already we are just pushing, so update doesn't touch it
+                end bracket (]) shows it is time to decompress current str
+                '''
             else:
                 ret +=ch
         return ret
