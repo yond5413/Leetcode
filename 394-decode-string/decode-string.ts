0788d90403cc584bf1d5a7d97289352d2c2875dc
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-11-26 11:10:06 -0500

    Time: 0 ms (100.00%) | Memory: 54.6 MB (75.00%) - LeetSync

--- 394-decode-string/decode-string.ts
+++ 394-decode-string/decode-string.ts
@@ -1,26 +1,26 @@
 function decodeString(s: string): string {
     let ret:string = ""
-    let strStack:string[] = []
-    let numStack:number[] = []
+    let num_stack:number[] = []
+    let str_stack:string[] = []
     let num:number = 0
-    const isDigit = (c:string):boolean => c>="0" && c<="9";
+    const isDigit = (ch:string):boolean => ch>="0" && ch<="9";
     for (let ch of s){
         if (isDigit(ch)){
             num = num*10 + Number(ch)
         }
-        else if(ch == "["){
-            numStack.push(num)
-            strStack.push(ret)
-            ret = ""
+        else if(ch === "["){
+            str_stack.push(ret)
+            num_stack.push(num)
             num = 0
+            ret = ""
         }
-        else if(ch == "]"){
-            let old_num = numStack.pop()
-            let old_str = strStack.pop()
-            ret = old_str+ret.repeat(old_num)
+        else if(ch === "]"){
+            let old_str = str_stack.pop()
+            let old_num = num_stack.pop()
+            ret = old_str + ret.repeat(old_num)
         }
         else{
-            ret+=ch
+            ret += ch
         }
     }
     return ret
