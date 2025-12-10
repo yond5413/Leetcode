dd441b107c83ba4679c220888a2ddac4a03f8b8f
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-12-10 16:39:43 -0500

    Time: 0 ms (100.00%) | Memory: 57.2 MB (75.52%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    if (root === null){
+        return []
+    }
+    let ret = []
+    let queue = [root]
+    while (queue.length>0){
+        let n = queue.length
+        for (let i =0;i<n;i++){
+            let curr = queue.shift()
+            if (i==0){
+                ret.push(curr.val)
+            }
+            if (curr.right != null){
+                queue.push(curr.right)
+            }
+             if (curr.left != null){
+                queue.push(curr.left)
+            }
+        }
+    }
+    return ret;
+};
