72bc18177423fe74e971a5f3d06659f6bd586f77
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-12-11 17:03:42 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (15.14%) - LeetSync

--- 1080-camelcase-matching/camelcase-matching.py
+++ 1080-camelcase-matching/camelcase-matching.py
@@ -1,15 +1,12 @@
 class Solution:
     def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
-        # Works because pattern is supposed to be in order
-        # We can only insert lowercases if there is a missing upper it is done
-        # If pattern was out of order we would need a hashmap to manage frequency
         def match(query,pattern):
             n = len(pattern)
             i = 0
             for c in query:
-                if i<n and c==pattern[i]:
+                if i<n and c == pattern[i]:
                     i+=1
                 elif c.isupper():
                     return False
-            return i==n
+            return i == n
         return [match(q,pattern) for q in queries]
