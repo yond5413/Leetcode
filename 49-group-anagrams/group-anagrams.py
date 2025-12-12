bf4c11ffe13c0101dc8ad901b908b3264e75c393
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-12-12 13:44:47 -0500

    Time: 11 ms (82.98%) | Memory: 20.6 MB (88.63%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        lookup = {}
+        for s in strs:
+            s_sorted = "".join(sorted(s))
+            if s_sorted in lookup:
+                lookup[s_sorted].append(s)
+            else:
+                lookup[s_sorted] = [s]
+
+        ret = []
+        for k in lookup.keys():
+            ret.append(lookup[k])
+        return ret
