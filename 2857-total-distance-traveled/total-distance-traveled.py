748c2ff3ca8f5f6bf84b6ffe8a9879fac340870b
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-12-16 21:25:21 -0500

    Time: 31 ms (20.18%) | Memory: 17.7 MB (89.91%) - LeetSync

--- 2857-total-distance-traveled/total-distance-traveled.py
+++ 2857-total-distance-traveled/total-distance-traveled.py
@@ -1,13 +1,13 @@
 class Solution:
     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
         ret = 0
-        used =0
-        while(mainTank>0):
-            used+=1
-            ret+=10
+        i = 0
+        while mainTank>0:
+            ret+= 10
+            i+=1
             mainTank-=1
-            if used == 5 and additionalTank>0:
-                used = 0
-                mainTank+=1
+            if i == 5 and additionalTank>0:
+                i=0
                 additionalTank-=1
+                mainTank+=1
         return ret
