305c683e5635a32948a6755dee60a085224007dd
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-12-16 22:53:32 -0500

    Time: 4 ms (51.20%) | Memory: 19.9 MB (74.74%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -2,12 +2,10 @@ import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         min_heap = []
-        intervals.sort(key=lambda x:x[0])
+        intervals.sort(key = lambda x:x[0])
         heapq.heappush(min_heap,intervals[0][1])
-        
         for i in range(1,len(intervals)):
             if min_heap and min_heap[0]<=intervals[i][0]:
                 heapq.heappop(min_heap)
             heapq.heappush(min_heap,intervals[i][1])
         return len(min_heap)
-            
