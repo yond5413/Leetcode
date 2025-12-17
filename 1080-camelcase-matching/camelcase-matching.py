f2cb2ff91e2333469bab4b6b6f3517b2b25078cd
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-12-17 10:14:04 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.12%) - LeetSync

--- 1080-camelcase-matching/camelcase-matching.py
+++ 1080-camelcase-matching/camelcase-matching.py
@@ -1,11 +1,11 @@
 class Solution:
     def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
-        def match(query,pattern):
+        def isMatch(query,pattern):
             i,n = 0,len(pattern)
             for ch in query:
                 if i<n and ch == pattern[i]:
                     i+=1
                 elif ch.isupper():
                     return False
-            return i==n
-        return [match(q,pattern) for q in queries]
+            return i ==n
+        return [isMatch(q,pattern) for q in queries]
