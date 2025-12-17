b42da4c06c1f435069cd39af9c2df7997ce9653e
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-12-16 21:07:24 -0500

    Time: 47 ms (37.58%) | Memory: 18.3 MB (16.49%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def isMatch(p,q):
+        def isSame(p,q):
             if not p and not q:
                 return True
             if not p or not q:
                 return False
-            return p.val == q.val and isMatch(p.left,q.left) and isMatch(p.right,q.right) 
+            return p.val == q.val and isSame(p.left,q.left)and isSame(p.right,q.right)
         def dfs(curr):
             if not curr:
                 return False
-            if isMatch(curr,subRoot):
+            if isSame(curr,subRoot):
                 return True
             return dfs(curr.left) or dfs(curr.right)
         return dfs(root)
