afa8a38183bfc395afd10a78f60e61c861d78f99
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-12-16 19:49:59 -0500

    Time: 263 ms (34.64%) | Memory: 25.5 MB (17.48%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
         ret = 0
+        visited = set()
         n,m = len(grid),len(grid[0])
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    stack = [(i,j)]
-                    ret+=1
+                if (i,j) not in visited and grid[i][j] == "1":
                     visited.add((i,j))
+                    ret+=1
+                    stack=[(i,j)]
                     while (stack):
                         x,y = stack.pop()
                         adj = [(0,1),(1,0),(0,-1),(-1,0)]
                         for a,b in adj:
                             cur_x,cur_y = x+a,y+b
-                            if (cur_x,cur_y) in visited:
-                                continue
-                            elif cur_x>=0 and cur_x<n and cur_y >= 0 and cur_y<m and grid[cur_x][cur_y]=="1":
-                                stack.append((cur_x,cur_y))
-                                visited.add((cur_x,cur_y))
-                        
+                            if cur_x>=0 and cur_x<n and cur_y>=0 and cur_y<m and grid[cur_x][cur_y] == "1":
+                                if (cur_x,cur_y) not in visited:
+                                    visited.add((cur_x,cur_y) )
+                                    stack.append((cur_x,cur_y))
         return ret
