ee26d8356764bcd7c428572ce146e71187e65fa4
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-02 23:39:27 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (80.38%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,12 +1,11 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         visited = set()
-        stack = [0]
-        while (stack):
-            curr = stack.pop()
+        queue = [0]
+        while queue:
+            curr = queue.pop()
             visited.add(curr)
             for i in rooms[curr]:
                 if i not in visited:
-                    stack.append(i)
-            
+                    queue.append(i)
         return len(rooms) == len(visited)
