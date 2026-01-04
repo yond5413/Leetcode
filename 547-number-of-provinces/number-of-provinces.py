4d4679186da9d4af5a4dbd66f22321c95981f2b4
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-04 17:57:36 -0500

    Time: 0 ms (100.00%) | Memory: 18.9 MB (62.13%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,8 +1,8 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        ret = 0
-        visited = set()
+        ret=0
         n = len(isConnected)
+        visited = set()
         def dfs(curr):
             visited.add(curr)
             for i in range(n):
