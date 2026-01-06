13b892b451ece6debe45abb4aebbef64e175a946
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-06 18:34:23 -0500

    Time: 3 ms (81.32%) | Memory: 19.4 MB (17.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ret,fresh = 0,0
+        ret = 0
+        queue = []
         m,n = len(grid),len(grid[0])
-        queue = list()
+        fresh = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] ==2:
-                    queue.append((i,j))
-                elif grid[i][j] == 1:
-                    fresh +=1
-        while(queue and fresh >0):
-            for _ in range(len(queue)):
-                x,y = queue.pop(0)
-                adj = [(1,0),(-1,0),(0,-1),(0,1)]
-                for i,j in adj:
-                    dx,dy = x+i,y+j
-                    if dx>=m or dx<0 or dy>=n or dy<0:
-                        continue
-                    
-                    if grid[dx][dy] == 1:
-                        fresh-=1
-                        grid[dx][dy] =2 
-                        queue.append((dx,dy))
-            ret +=1
+                if grid[i][j] == 2:
+                    queue.append((i,j,1))
+                elif grid[i][j] == 1 :
+                    fresh+=1
+       #######################################
+        while (queue and fresh>0):
+            i,j,step = queue.pop(0)
+            adj = [(1,0),(-1,0),(0,1),(0,-1)]
+            for x,y in adj:
+                dx,dy = x+i,y+j
+                if dx>=0 and dx<m and dy>=0 and dy<n and grid[dx][dy] == 1:
+                        queue.append((dx,dy,step+1))
+                        grid[dx][dy] = 2
+                        fresh -=1
+            ret =max(ret,step)
         return ret if fresh == 0 else -1
+
+
+
+
