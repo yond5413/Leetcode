23afde43bd3ebdfc2ba2c453d57ca64a81677e4d
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-06 17:28:50 -0500

    Time: 215 ms (6.40%) | Memory: 21.8 MB (7.52%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -1,21 +1,23 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        m,n = len(maze),len(maze[0])
-        ret = float("inf")
         u,v = entrance
+        m,n = len(maze),len(maze[0])
+        visit = set()
         queue = [(u,v,0)]
-        visited = set()
+        ret = float("inf")
         while queue:
             i,j,steps = queue.pop(0)
+            if maze[i][j] == "+":
+                continue
+            if i == 0 or i == m-1 or j == 0 or j == n-1:
+                ret = min(ret,steps) if [i,j] !=entrance else ret
             adj = [(1,0),(-1,0),(0,1),(0,-1)]
-            if i == 0 or i == m-1 or j == 0 or j == n-1 and maze[i][j] == ".":
-                
-                ret = min(steps,ret) if [i,j] != entrance else ret
             for x,y in adj:
                 d_x,d_y = i+x,j+y
                 if d_x>=0 and d_x<m and d_y>=0 and d_y<n:
-                    if (d_x,d_y) not in visited and maze[d_x][d_y] == ".":
+                    if (d_x,d_y) not in visit and maze[d_x][d_y] == ".":
                         queue.append((d_x,d_y,steps+1))
-                        visited.add((d_x,d_y))
-            visited.add((i,j))
+                        visit.add((d_x,d_y))
+            visit.add((i,j))
+
         return ret if ret != float("inf") else -1
