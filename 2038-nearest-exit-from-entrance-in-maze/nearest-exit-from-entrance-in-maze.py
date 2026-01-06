2cd5f6e30fa53c26664eb691b1f5674531c6d521
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-06 17:55:45 -0500

    Time: 207 ms (6.86%) | Memory: 22.1 MB (7.52%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -1,23 +1,22 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        ret = float("inf")
         u,v = entrance
         m,n = len(maze),len(maze[0])
-        visit = set()
         queue = [(u,v,0)]
-        ret = float("inf")
+        visit = set()
         while queue:
             i,j,steps = queue.pop(0)
             if maze[i][j] == "+":
                 continue
-            if i == 0 or i == m-1 or j == 0 or j == n-1:
-                ret = min(ret,steps) if [i,j] !=entrance else ret
+            if  i == 0 or i == m-1 or j == 0 or j == n-1 :  
+                ret = min(ret,steps) if [i,j] != entrance else ret
             adj = [(1,0),(-1,0),(0,1),(0,-1)]
-            for x,y in adj:
-                d_x,d_y = i+x,j+y
-                if d_x>=0 and d_x<m and d_y>=0 and d_y<n:
-                    if (d_x,d_y) not in visit and maze[d_x][d_y] == ".":
-                        queue.append((d_x,d_y,steps+1))
-                        visit.add((d_x,d_y))
             visit.add((i,j))
-
+            for x,y in adj:
+                dx,dy = i+x,j+y
+                if dx >=0 and dx <m and dy>=0 and dy<n and maze[dx][dy] == ".":
+                    if (dx,dy) not in visit:
+                        queue.append((dx,dy,steps+1))
+                        visit.add((dx,dy))
         return ret if ret != float("inf") else -1
