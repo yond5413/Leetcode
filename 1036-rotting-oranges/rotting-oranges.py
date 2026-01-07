e129a3c040b3cb96e524b433171e6e913e23287e
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-07 12:46:02 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (17.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,25 +1,25 @@
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ret = 0
-        fresh = 0
-        m,n  = len(grid),len(grid[0])
+        ret,fresh = 0,0
         queue = []
+        m,n = len(grid),len(grid[0])
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     queue.append((i,j,1))
-                elif grid[i][j] ==1:
+                elif grid[i][j] == 1:
                     fresh+=1
-        #######################################
-        while (queue and fresh>0):
+        while queue and fresh>0:
             i,j,step = queue.pop(0)
             adj = [(1,0),(-1,0),(0,1),(0,-1)]
             for x,y in adj:
-                dx,dy = x+i,y+j
-                if dx>=0 and dx<m and dy>=0 and dy<n:
-                    if grid[dx][dy] ==1:
+                dx,dy = i+x,j+y
+                if dx >=0 and dx<m and dy>=0 and dy<n:
+                    if grid[dx][dy] == 1:
                         fresh-=1
                         queue.append((dx,dy,step+1))
                         grid[dx][dy] = 2
-            ret = max(step,ret)
-        return ret if fresh == 0 else -1
+            ret = max(ret,step)
+
+        return ret if fresh ==0 else -1
