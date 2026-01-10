a678e38c8ba582b75bfd56f82ccb63fa9320abd7
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-09 19:53:07 -0500

    Time: 131 ms (60.77%) | Memory: 29.2 MB (42.28%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -1,21 +1,19 @@
-import heapq
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
         q1,q2 = [],[]
-        l,r = 0,len(costs)-1
         ret = 0
-        for i in range(k):
-            while len(q1)< candidates  and l<=r:
+        l,r = 0,len(costs)-1
+        for _ in range(k):
+            while len(q1)<candidates and l<=r:
                 heapq.heappush(q1,costs[l])
                 l+=1
-            while len(q2)< candidates  and l<=r:
+            while len(q2)<candidates and l<=r:
                 heapq.heappush(q2,costs[r])
                 r-=1
-            val1 = q1[0] if q1 else float("inf")
-            val2 = q2[0] if q2 else float("inf")
-            if val1<=val2:
-                ret+=heapq.heappop(q1)
+            v1 = q1[0] if q1 else float("inf")
+            v2 = q2[0] if q2 else float("inf")
+            if v1<=v2:
+                ret += heapq.heappop(q1)
             else:
-                ret+=heapq.heappop(q2)
-
+                ret += heapq.heappop(q2)
         return ret
