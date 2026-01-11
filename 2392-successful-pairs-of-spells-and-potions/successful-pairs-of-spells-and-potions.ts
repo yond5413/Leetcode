785ca5de2682c41b2775d60ff6cd5307ffefa5fa
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-11 12:19:02 -0500

    Time: 111 ms (60.64%) | Memory: 87.8 MB (38.71%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.ts
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.ts
@@ -2,22 +2,21 @@ function successfulPairs(spells: number[], potions: number[], success: number): 
     let n = spells.length
     let m = potions.length
     let ret = []
-   potions.sort((a,b)=>a-b)
-    
+    potions.sort((a,b)=>a-b)
     for (let i = 0; i<n;i++){
         let l = 0
         let r = m-1
         while(l<=r){
             let j = Math.floor((l+r)/2)
-            if (spells[i]*potions[j] >= success){
-                 r = j-1
+            if (spells[i]*potions[j]>=success){
+                r = j-1
             }
             else{
-               
                 l = j+1
             }
         }
         ret.push(m-l)
+
     }
     return ret
 };
