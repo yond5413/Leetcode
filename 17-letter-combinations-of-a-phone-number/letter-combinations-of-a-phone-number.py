ac2d706b09cc64ab49ed6819d3b48811231ab5fd
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-17 16:39:45 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (15.99%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,26 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if digits == '':
-            return []
-        hashmap = {
-        '2':['a','b','c'],
-        '3':['d','e','f'],
-        '4':['g','h','i'],
-        '5':['j','k','l'],
-        '6':['m','n','o'],
-        '7':['p','q','r','s'],
-        '8':['t','u','v'],
-        '9':['w','x','y','z']
-        }
         ret = []
-        def dfs(cur,idx):
-            if idx == len(digits):
-                ret.append(''.join(cur[:]))
+        lookup = {
+            '2':["a","b","c"],
+            '3':["d","e","f"],
+            '4':["g","h","i"],
+            '5':["j","k","l"],
+            '6':["m","n","o"],
+            '7':["p","q","r","s"],
+            '8':["t","u","v"],
+            '9':["w","x","y","z"],
+        }
+        visited = set()
+        n = len(digits)
+        def dfs(idx,curr):
+            if idx ==n and curr not in visited:
+                visited.add(curr)
+                ret.append(curr[::])
                 return 
-            chars = hashmap[digits[idx]]
-            for i in chars:
-                cur.append(i)
-                dfs(cur,idx+1)
-                cur.pop()
-        dfs([],0)
+            paths = lookup[digits[idx]]
+            for p in paths:
+                if curr+p not in visited:
+                    dfs(idx+1,curr+p)
+        dfs(0,"")
         return ret
