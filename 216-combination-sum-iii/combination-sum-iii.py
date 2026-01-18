2eb09ec7394449f3c612b06888cc5fcacb2d7183
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-18 10:37:01 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (14.54%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,12 +1,13 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         ret = []
-        def dfs(curr,tot,idx):
-            if len(curr) == k and tot == n:
-                ret.append(curr[:])
-                return  
+        #visited = set()
+        def dfs(idx,curr,tot):
+            if len(curr) ==k and tot == n:
+                ret.append(curr[::])
+                return
             for i in range(idx,10):
-                if tot+i <= n:
-                    dfs(curr+[i],tot+i,i+1)
-        dfs([],0,1)
+                if tot+i<=n:
+                    dfs(i+1,curr+[i],tot+i)       
+        dfs(1,[],0)
         return ret
