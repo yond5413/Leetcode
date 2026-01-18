cf92996ddc4bf9b8f323bd05957e984f534ff0b9
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-18 10:26:16 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (8.63%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,25 +2,26 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         ret = []
         lookup = {
-            '2':["a","b","c"],
-            '3':["d","e","f"],
-            '4':["g","h","i"],
-            '5':["j","k","l"],
-            '6':["m","n","o"],
-            '7':["p","q","r","s"],
-            '8':["t","u","v"],
-            '9':["w","x","y","z"],
+            "2":["a","b","c"],
+            "3":["d","e","f"],
+            "4":["g","h","i"],
+            "5":["j","k","l"],
+            "6":["m","n","o"],
+            "7":["p","q","r","s"],
+            "8":["t","u","v"],
+            "9":["w","x","y","z"],
         }
         visited = set()
         n = len(digits)
         def dfs(idx,curr):
-            if idx ==n and curr not in visited:
-                visited.add(curr)
+            if idx == n:
                 ret.append(curr[::])
-                return 
+                return
             paths = lookup[digits[idx]]
             for p in paths:
                 if curr+p not in visited:
+                    visited.add(curr+p)
                     dfs(idx+1,curr+p)
+                    visited.remove(curr+p)
         dfs(0,"")
         return ret
