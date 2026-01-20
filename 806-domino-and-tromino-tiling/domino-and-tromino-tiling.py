252c94b7f46328e0c8193387f7933c06ff7042f2
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-20 16:03:39 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.53%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        
+        if n<3:
+            return n
+        dp = [0]*n
+        dp[0],dp[1],dp[2] = 1,2,5
+        for i in range(3,n):
+            dp[i] = 2*dp[i-1]+dp[i-3]
+        return dp[n-1]%(7+10**9)
