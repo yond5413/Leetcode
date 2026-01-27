c3c71a67a616f8a2291664b5c6b8f99fd0c2ae63
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-27 14:28:54 -0500

    Time: 0 ms (100.00%) | Memory: 23 MB (26.11%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,6 @@
-from collections import Counter
 import heapq
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
-        return heapq.nlargest(k,freq,key = freq.get)
+        return nlargest(k,freq,key = freq.get)
