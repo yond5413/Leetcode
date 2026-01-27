c7af8649eb93b355aaca943c91e064ef8f23dd75
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-01-27 14:23:59 -0500

    Time: 11 ms (83.43%) | Memory: 22.1 MB (38.01%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         lookup = defaultdict(list)
-        for s in strs:
-            s_sorted = "".join(sorted(s))
-            lookup[s_sorted].append(s)
+        for i in range(len(strs)):
+            s_sorted = "".join(sorted(strs[i]))
+            lookup[s_sorted].append(strs[i])
         return [lookup[k] for k in lookup.keys()]
+
