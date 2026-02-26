342337853db80af7711a8ded79cf1037f8c32f16
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-02-26 13:09:35 -0500

    Time: 0 ms (100.00%) | Memory: 21.6 MB (6.12%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not n:
+            return True
+        adj = defaultdict(list)
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        ## no cycles or repeating edges?
+        ## connected undirected graph without "simple" cycles
+        visited = set()
+        def dfs(curr,prev):
+            if curr in visited:
+                return False
+            visited.add(curr)
+            for i in adj[curr]:
+                if i == prev:
+                    continue
+                if not dfs(i,curr):
+                    return False
+            
+            return True
+        
+        return dfs(0,-1) and len(visited) == n
+
+           
