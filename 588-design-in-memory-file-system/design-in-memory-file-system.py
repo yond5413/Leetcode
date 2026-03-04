23e6896a22311b485d693c150f32f608206f849f
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-03-04 12:43:25 -0500

    Time: 0 ms (100.00%) | Memory: 19.8 MB (69.02%) - LeetSync

--- 588-design-in-memory-file-system/design-in-memory-file-system.py
+++ 588-design-in-memory-file-system/design-in-memory-file-system.py
@@ -5,15 +5,15 @@ class Node:
         self.content = []
         self.children = {}
     def insert(self,path,isFile):
-        dirs = path.split("/")
         node = self
-        for i in dirs[1:]:
-            if i not in node.children:
-                node.children[i] = Node()
-            node = node.children[i]
+        path_entry = path.split("/")
+        for entry in path_entry[1:]:
+            if entry not in node.children:
+                node.children[entry] = Node()
+            node = node.children[entry]
         node.isFile = isFile
         if node.isFile:
-            node.name = dirs[-1]
+            node.name = path_entry[-1]
         return node
     def search(self,path):
         node = self
@@ -25,30 +25,29 @@ class Node:
                 return None
             node = node.children[entry]
         return node
-
 class FileSystem:
 
     def __init__(self):
         self.root = Node()
 
     def ls(self, path: str) -> List[str]:
-        node = self.root.search(path)
-        if node is None:
+        res = self.root.search(path)
+        if not res:
             return []
-        if node.isFile:
-            return [node.name]
-        return sorted(node.children.keys())
+        if res.isFile:
+            return [res.name]
+        return sorted(res.children.keys())
 
     def mkdir(self, path: str) -> None:
-        self.root.insert(path,isFile=False)
+        self.root.insert(path,False)
 
     def addContentToFile(self, filePath: str, content: str) -> None:
-        node = self.root.insert(filePath,isFile=True)
-        node.content.append(content)
+        res = self.root.insert(filePath,True)
+        res.content.append(content)
 
     def readContentFromFile(self, filePath: str) -> str:
-        node = self.root.search(filePath)
-        return "".join(node.content)
+        return "".join(self.root.search(filePath).content)
+        
 
 
 # Your FileSystem object will be instantiated and called as such:
