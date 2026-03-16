92338927f1ffcf1640b6714d5a892dbe26bb4964
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-03-16 11:10:49 -0400

    Time: 53 ms (72.35%) | Memory: 57.2 MB (20.77%) - LeetSync

---
+++ 2741-function-composition/function-composition.ts
@@ -0,0 +1,17 @@
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    
+    return function(x) {
+        let ret = x
+        for (let i =functions.length-1;i>=0;i--){
+            ret = functions[i](ret)
+        }
+        return ret
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
