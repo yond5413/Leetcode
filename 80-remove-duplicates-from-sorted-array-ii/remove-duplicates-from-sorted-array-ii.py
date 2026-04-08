ad0623e5556414525d2a597947c9cceed6654bb2
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-04-08 13:15:13 -0400

    Time: 90 ms (41.88%) | Memory: 21.9 MB (77.00%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -1,20 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        '''
-        - each entry can appear at most twice
-        - still sorted?
-        -> sliding window approach
-        '''
-        '''
-        sliding window
-        -->we need left pointer(k) to be at least 2 to check l-2
-        -> we then check if l-2 is != nums[r] via right pointer so we can overwrite it
-        ---> this is because we can have 2 entires in a rowm but we just want to slide valid 
-        entries to the left return l as our new length
-        '''
-        l = 2
+        l=2
         for r in range(2,len(nums)):
             if nums[l-2] != nums[r]:
                 nums[l] = nums[r]
                 l+=1
         return l
+        
