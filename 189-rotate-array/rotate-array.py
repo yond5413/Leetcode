0fa653981741028650df6305d740039b538d28b6
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-04-09 14:26:46 -0400

    Time: 14 ms (13.36%) | Memory: 26.6 MB (32.40%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,16 +5,15 @@ class Solution:
         """
         n = len(nums)
         steps = k%n
-        if steps == 0:
-            return 
+        if steps ==0:
+            return
         def rev(arr,l,r):
-            while(l<r):
+            while (l<r):
                 arr[l],arr[r] = arr[r],arr[l]
                 l+=1
                 r-=1
             return arr
         nums = rev(nums,0,n-1)
-        #print(nums)
+        print(nums)
         nums = rev(nums,0,steps-1)
-        #print(nums)
         nums = rev(nums,steps,n-1)
