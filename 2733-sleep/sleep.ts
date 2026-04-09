3c764c6d5b14b97d3cacbc8f97f202d0185024e8
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-04-09 17:40:21 -0400

    Time: 41 ms (76.77%) | Memory: 55.5 MB (20.50%) - LeetSync

--- 2733-sleep/sleep.ts
+++ 2733-sleep/sleep.ts
@@ -1,8 +1,7 @@
 async function sleep(millis: number): Promise<void> {
-    return new Promise(resolve=>{
+    return new Promise((resolve)=>{
         setTimeout(resolve,millis)
     })
-    
 }
 
 
