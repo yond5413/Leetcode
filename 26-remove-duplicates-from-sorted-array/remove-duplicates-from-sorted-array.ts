d1307e0855c338b7258a1821abf08ee682b9d862
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-12 12:38:16 -0400

    Time: 0 ms (100.00%) | Memory: 58.7 MB (51.79%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,12 +1,13 @@
 function removeDuplicates(nums: number[]): number {
     let k = 0
-    for (let i=0;i<nums.length;i++){
-        if (i===0 || nums[i-1]===nums[i]){
-            k = i===0 ? k+1: k
-            continue 
+    for(let i =0;i<nums.length;i++){
+        if (i===0 || nums[i-1] ===nums[i]){
+            k = i===0 ? k+1 : k
+            continue
         }
         nums[k] = nums[i]
         k++
     }
+
     return k
 };
