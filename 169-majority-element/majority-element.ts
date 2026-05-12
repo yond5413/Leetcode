fbb698af6d304f73676fe902b3b2926653a69ebf
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-12 14:13:18 -0400

    Time: 3 ms (64.82%) | Memory: 58.2 MB (63.37%) - LeetSync

--- 169-majority-element/majority-element.ts
+++ 169-majority-element/majority-element.ts
@@ -1,19 +1,18 @@
 function majorityElement(nums: number[]): number {
     let freq = new Map()
-    for (let i = 0; i<nums.length;i++){
+    const n  =nums.length
+    for (let i = 0;i<n;i++){
         if (freq.has(nums[i])){
-            freq.set(nums[i], freq.get(nums[i])+1)
+            freq.set(nums[i],freq.get(nums[i])+1)
         }
         else{
             freq.set(nums[i],1)
+
         }
     }
-const n  = nums.length
     for (const k of freq.keys()){
-        //console.log(freq[k])
         if (freq.get(k) >= n/2){
             return k
         }
     }
-
-}
+};
