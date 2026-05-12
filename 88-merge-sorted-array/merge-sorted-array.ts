1b85c7dfdfb3659ce4db5edc52be4830b667d7db
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-12 11:45:49 -0400

    Time: 0 ms (100.00%) | Memory: 55.8 MB (74.70%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.ts
+++ 88-merge-sorted-array/merge-sorted-array.ts
@@ -1,9 +1,11 @@
 /**
  Do not return anything, modify nums1 in-place instead.
  */
+
+ 
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let [i,j,k] = [m-1,n-1,(m+n)-1]
-    while (j>=0){
+    let [i,j,k] = [m-1,n-1,m+n-1]
+    while(j>=0){
         if (nums1[i]>=nums2[j] && i>=0){
             nums1[k] = nums1[i]
             i--
