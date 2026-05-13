d37fd879775ea489598b59c5c184a2465fe93f75
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-13 14:44:02 -0400

    Time: 15 ms (12.10%) | Memory: 26.9 MB (14.60%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,16 +4,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        steps = k%n
-        if steps ==0:
-            return
-        def rev(arr,l,r):
-            while (l<r):
-                arr[l],arr[r] = arr[r],arr[l]
-                l+=1
-                r-=1
+        rot = k%n
+        if rot == 0:
+            return 
+        
+        def help(arr,l,r):
+            i,j = l,r
+            print(f"i:{i}, j:{j}")
+            while(i<j):
+                arr[i],arr[j] = arr[j],arr[i]
+                i+=1
+                j-=1
+            print(arr)
             return arr
-        nums = rev(nums,0,n-1)
-        print(nums)
-        nums = rev(nums,0,steps-1)
-        nums = rev(nums,steps,n-1)
+        nums = help(nums,0,n-1)
+        nums = help(nums,0,rot-1)
+        nums = help(nums,rot,n-1)
+        return
