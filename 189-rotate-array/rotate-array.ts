ab4877b0c0b3d850be2496b2a550c7c61c3dcf03
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-13 14:58:12 -0400

    Time: 8 ms (30.92%) | Memory: 66.5 MB (53.16%) - LeetSync

--- 189-rotate-array/rotate-array.ts
+++ 189-rotate-array/rotate-array.ts
@@ -2,19 +2,23 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-  const n = nums.length
-  const steps = k%n  
-  if (steps===0) return;
-    const rev = (arr:number[],l:number,r:number)=>{   
+    const n = nums.length
+    const rot = k%n
+    if (rot == 0) return 
+
+    let helper = (arr:number[],l:number,r: number) :number[]=>{
         while(l<r){
-            [nums[l],nums[r]] = [nums[r],nums[l]]
+            let temp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = temp
             l++
             r--
+            
         }
-
+        console.log(arr)
         return arr
     }
-    nums = rev(nums,0,n-1)
-    nums = rev(nums,0,steps-1)
-    nums = rev(nums,steps,n-1)
+    nums = helper(nums,0,n-1)
+    nums = helper(nums,0,rot-1)
+    nums = helper(nums,rot,n-1)
 };
