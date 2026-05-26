383361bca884c4d1f71c2bab051aaa7a7103f6fb
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-26 16:09:08 -0400

    Time: 258 ms (32.09%) | Memory: 26.2 MB (27.71%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,11 +2,11 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n = len(grid),len(grid[0])
         visited = set()
-        ret = 0
+        ret =0
         for i in range(m):
             for j in range(n):
-                if (i,j) not in visited and grid[i][j]=="1":
-                    ret +=1
+                if (i,j) not in visited and grid[i][j] == "1":
+                    ret+=1
                     queue = [(i,j)]
                     visited.add((i,j))
                     while queue:
@@ -14,10 +14,8 @@ class Solution:
                         adj = [(0,1),(1,0),(0,-1),(-1,0)]
                         for u,v in adj:
                             dx,dy = x+u,y+v
-                            if dx>=0 and dx<m and dy>=0 and dy<n:
-                                if grid[dx][dy]=="1" and (dx,dy) not in visited:
-                                    queue.append((dx,dy))
+                            if dx>=0 and dx<m and dy>=0 and dy<n and grid[dx][dy]== "1":
+                                if (dx,dy) not in visited:
                                     visited.add((dx,dy))
-                            
-
+                                    queue.append((dx,dy))
         return ret
