54ada5574d9f68b1dc247e001f6a044843e7691b
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-26 16:50:00 -0400

    Time: 23 ms (43.51%) | Memory: 19.4 MB (89.89%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,24 +1,23 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m,n = len(grid),len(grid[0])
         ret = 0
         visited = set()
+        m,n = len(grid),len(grid[0])
         for i in range(m):
             for j in range(n):
                 if (i,j) not in visited and grid[i][j] == 1:
                     queue = [(i,j)]
                     visited.add((i,j))
                     curr = 1
-                    while queue:
+                    while (queue):
                         x,y = queue.pop(0)
                         adj = [(1,0),(0,1),(-1,0),(0,-1)]
                         for u,v in adj:
                             dx,dy = x+u,y+v
-                            if dx>=0 and dx<m and dy<n and dy>=0 and grid[dx][dy] == 1:
+                            if dx>=0 and dx<m and dy>=0 and dy<n and grid[dx][dy]==1:
                                 if (dx,dy) not in visited:
-                                    curr +=1
+                                    curr+=1
                                     visited.add((dx,dy))
                                     queue.append((dx,dy))
-                        ret = max(ret,curr)
-
+                    ret = max(ret,curr)
         return ret
