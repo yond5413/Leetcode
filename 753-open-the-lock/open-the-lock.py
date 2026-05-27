8a5e7576c0b09f56b441b527aebb1801bf54a43c
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-27 16:01:22 -0400

    Time: 348 ms (50.81%) | Memory: 20.5 MB (29.09%) - LeetSync

--- 753-open-the-lock/open-the-lock.py
+++ 753-open-the-lock/open-the-lock.py
@@ -1,25 +1,24 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        # start -> "0000"
-        if "0000" in deadends:
+        dead_set = set(deadends)
+        if "0000" in dead_set:
             return -1
         queue = [("0000",0)]
         visited = set(["0000"])
         while queue:
             combo,steps = queue.pop(0)
             if combo == target:
-                #probably
-                return steps 
-            if combo in deadends:
+                return steps
+            if combo in dead_set:
                 continue
             for i in range(len(combo)):
                 val = int(combo[i])
                 v1 = val+1 if val!=9 else 0
                 v2 = val-1 if val!=0 else 9
-                turns = [v1,v2]
-                for t in turns:
-                    path = combo[:i]+str(t)+combo[i+1:]
-                    if path not in visited:
-                        queue.append((path,steps+1))
-                        visited.add(path)
+                paths = [v1,v2]
+                for t in paths:
+                    curr = combo[:i]+ str(t) + combo[i+1:]
+                    if curr not in visited:
+                        queue.append((curr,steps+1))
+                        visited.add(curr)
         return -1
