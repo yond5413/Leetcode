687da164dfb6f4123db34c7049838d1644d5017e
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-27 15:26:55 -0400

    Time: 128 ms (62.80%) | Memory: 23.8 MB (15.07%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,27 +1,27 @@
-from collections import defaultdict 
+from collections import defaultdict
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
-        
         if endWord not in wordList:
             return 0
+        
         hashmap = defaultdict(list)
-        queue = [(beginWord,1)]
-        visited = set([beginWord])
-
+        
         for i in range(len(wordList)):
             for j in range(len(wordList[i])):
-                new = wordList[i][:j]+"*"+ wordList[i][j+1:]
-                hashmap[new].append(wordList[i])
-        
-        while (queue):
+                curr = wordList[i][:j] +"*" + wordList[i][j+1:]
+                hashmap[curr].append(wordList[i])
+        queue = [(beginWord,1)]
+        visited = set([beginWord])
+        while queue:
             word,steps = queue.pop(0)
             if word == endWord:
                 return steps
             for i in range(len(word)):
-                curr = word[:i] +"*"+word[i+1:]
+                curr = word[:i] + "*" + word[i+1:]
                 if curr in hashmap and curr not in visited:
                     visited.add(curr)
                     for neigh in hashmap[curr]:
                         queue.append((neigh,steps+1))
         return 0
+
+        
