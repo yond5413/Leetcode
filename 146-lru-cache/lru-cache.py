b18c749f00291919f603170c4ba11b94139e941d
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-28 09:45:02 -0400

    Time: 175 ms (9.71%) | Memory: 78.9 MB (7.31%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -4,44 +4,53 @@ class ListNode:
         self.val = val
         self.prev = prev
         self.next = next
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.lookup = {}
-        self.head = ListNode()
-        self.tail = ListNode()
+        self.cache = {}
+        self.cap = capacity
+        self.head,self.tail = ListNode(),ListNode()
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.cap = capacity
-    ##########################################
-    # LinkedList helpers
+    #########################################
     def insert(self,node):
-        prev,next = self.tail.prev,self.tail
+        prev,next = self.head,self.head.next
+        node.prev,node.next = prev,next
         prev.next = node
         next.prev = node
-        node.next,node.prev = next,prev
     def remove(self,node):
         prev,next = node.prev,node.next
         prev.next = next
         next.prev = prev
-    ##########################################
+
+    #########################################
     def get(self, key: int) -> int:
-        if key not in self.lookup:
+        if key not in self.cache:
             return -1
-        curr = self.lookup[key]
+        curr = self.cache[key]
         self.remove(curr)
         self.insert(curr)
         return curr.val
+        
     def put(self, key: int, value: int) -> None:
-        if key in self.lookup:
-            self.remove(self.lookup[key])
-        new_node = ListNode(key,value)
-        self.lookup[key] = new_node
-        self.insert(new_node)
-        if len(self.lookup) > self.cap:
-            evict = self.head.next
+        if key in self.cache:
+            self.remove(self.cache[key])
+        node = ListNode(key=key,val=value)
+        self.cache[key] = node
+        self.insert(node)
+        if self.cap <len(self.cache):
+            evict = self.tail.prev
             self.remove(evict)
-            self.lookup.pop(evict.key)
+            self.cache.pop(evict.key)
+
+
+
+        
+    
+
+        
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
