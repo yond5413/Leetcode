3ea03a027451b864a1fd5c9572ef46da5c76f815
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-28 08:53:57 -0400

    Time: 11 ms (25.06%) | Memory: 22.4 MB (85.74%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,8 +4,8 @@ class Solution:
         ret = []
         curr = intervals[0]
         for i in range(1,len(intervals)):
-            if curr[0]<=intervals[i][0] and curr[1]>= intervals[i][0]:
-                curr[1] = intervals[i][1] if intervals[i][1]>curr[1] else curr[1]
+            if curr[0]<= intervals[i][0] and curr[1]>= intervals[i][0]:
+                curr[1] = intervals[i][1] if intervals[i][1]>=curr[1] else curr[1]
             else:
                 ret.append(curr)
                 curr = intervals[i]
