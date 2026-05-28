37e6baf71025513d3b3ef764288592296bd6a5c4
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-28 11:53:05 -0400

    Time: 8 ms (5.85%) | Memory: 19.3 MB (40.96%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,27 +1,25 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        fresh,rotten = 0,[]
         m,n = len(grid),len(grid[0])
-        ret = 0
-        visited = set()
-        rotten,fresh = [],0
-
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:
-                    fresh+=1
+                if grid[i][j]==1:
+                    fresh +=1
                 elif grid[i][j] ==2:
                     rotten.append((i,j))
-        queue = [(i,j,0) for i,j in rotten]
-
-        while(queue and fresh):
-            x,y,t = queue.pop(0)
-            adj = [(1,0),(0,1),(-1,0),(0,-1)]
+        queue = [(i,j,1) for i,j in rotten]
+        visited = set(rotten)
+        ret = 0
+        while queue and fresh>0:
+            x,y,steps = queue.pop(0)
+            ret = max(ret,steps)
+            adj = [(0,1),(1,0),(0,-1),(-1,0)]
             for i,j in adj:
-                dx,dy = x+i, y+j
-                if dx>=0 and dx<m and dy>=0 and dy<n and grid[dx][dy] ==1:
-                    if (dx,dy) not in visited:
-                        visited.add((dx,dy))
-                        queue.append((dx,dy,t+1))
-                        fresh-= 1
-            ret = max(ret,t+1)
+                dx,dy = x+i,y+j
+                if dx>=0 and dx<m and dy>=0 and dy<n and (dx,dy) not in visited and grid[dx][dy]==1:
+                    queue.append((dx,dy,steps+1))
+                    visited.add((dx,dy))
+                    fresh-=1
         return ret if fresh == 0 else -1
+
