39d6461d3ed3a9baba6a7e3b36ff5ca05e0d0219
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-05-27 20:32:26 -0400

    Time: 347 ms (37.62%) | Memory: 22 MB (8.46%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -2,14 +2,9 @@ from collections import defaultdict
 import heapq
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # u->v with some w
-        # n nodes
-        # k = start 
-        # djkstra algo
-        adj = defaultdict(list)
+        edges = defaultdict(list)
         for u,v,w in times:
-            adj[u].append((w,v))
-
+            edges[u].append((w,v))
         visited = set()
         min_heap = [(0,k)]
         t = 0
@@ -19,11 +14,7 @@ class Solution:
                 continue
             t = max(t,w1)
             visited.add(v1)
-            for w2,v2 in adj[v1]:
+            for w2,v2 in edges[v1]:
                 if v2 not in visited:
                     heapq.heappush(min_heap,(w1+w2,v2))
-                
-        print(f"{len(visited)}, n:{n}")
         return t if n == len(visited) else -1
-
-
