901c1e0a7a1bca476e5f30d705c3cb92c760ffe9
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-06-21 14:18:51 -0400

    Time: 363 ms (5.01%) | Memory: 28.7 MB (41.55%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ret = float("-inf")
+        ret = 0
         l = prices[0]
-        for i in range(1,len(prices)):
-            #print(f"ret:{ret}, curr:{prices[i]-l}")
-            ret = max(ret,prices[i]-l)
-            
-            l  = min(l,prices[i])
-
-        return ret if ret>0 else 0
+        for r in range(1,len(prices)):
+            print(f"l: {l}, prices[r]: {prices[r]}")
+            ret = max(ret,prices[r]-l)
+            l = min(prices[r],l)
+        
+        return ret if ret !=float("-inf") else 0 
