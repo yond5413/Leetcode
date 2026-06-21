5763689de716d9addb621617c2b7c8b1ef31e555
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-06-21 14:34:13 -0400

    Time: 191 ms (7.71%) | Memory: 78.7 MB (16.53%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,46 +1,45 @@
-class ListNode:
-    def __init__(self,key=0,val=0,prev=None,next=None):
+class Node: 
+    def __init__(self,key = 0,val=0,next=None,prev=None):
         self.key = key
         self.val = val
-        self.prev = prev
         self.next = next
+        self.prev = prev
 class LRUCache:
-
+    
     def __init__(self, capacity: int):
-        self.cache = {}
         self.cap = capacity
-        self.head,self.tail = ListNode(),ListNode()
+        self.lookup = {} 
+        self.head,self.tail = Node(),Node()
         self.head.next = self.tail
         self.tail.prev = self.head
-    ###################################
-    def insert(self,node):
+    ##########################
+    def insert(self,curr):
         prev,next = self.head,self.head.next
-        node.prev,node.next = prev,next
-        prev.next = node
-        next.prev = node
-    def remove(self,node):
-        prev,next = node.prev,node.next
+        curr.prev,curr.next = prev,next
+        prev.next = curr
+        next.prev = curr
+    def remove(self,curr):
+        prev,next = curr.prev,curr.next
         prev.next = next
         next.prev = prev
-    ###################################
+    ##########################
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.lookup:
             return -1
-        curr = self.cache[key]
+        curr = self.lookup[key]
         self.remove(curr)
         self.insert(curr)
         return curr.val
-
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        newNode = ListNode(key,value)
-        self.cache[key] = newNode
+        if key in self.lookup:
+            self.remove(self.lookup[key])
+        newNode = Node(key,value)
+        self.lookup[key] = newNode
         self.insert(newNode)
-        if len(self.cache)>self.cap:
-            evict = self.tail.prev
-            self.remove(evict)
-            self.cache.pop(evict.key)
+        if self.cap <len(self.lookup):
+            lru = self.tail.prev
+            self.lookup.pop(lru.key)
+            self.remove(lru)
 
 
 # Your LRUCache object will be instantiated and called as such:
