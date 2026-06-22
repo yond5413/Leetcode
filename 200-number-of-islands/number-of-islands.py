1da0acd51ff2cb3f8d0f615d5ca86fcbd5d6afb6
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-06-21 21:12:59 -0400

    Time: 256 ms (33.79%) | Memory: 26.2 MB (27.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,21 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m,n = len(grid),len(grid[0])
+        ret = 0
         visited = set()
-        ret =0
+        m,n = len(grid),len(grid[0])
         for i in range(m):
             for j in range(n):
-                if (i,j) not in visited and grid[i][j] == "1":
+                if  grid[i][j] == "1" and (i,j) not in visited:
                     ret+=1
-                    queue = [(i,j)]
                     visited.add((i,j))
+                    queue = [(i,j)]
+                    adj = [(0,1),(1,0),(0,-1),(-1,0)]
                     while queue:
                         x,y = queue.pop(0)
-                        adj = [(0,1),(1,0),(0,-1),(-1,0)]
-                        for u,v in adj:
-                            dx,dy = x+u,y+v
-                            if dx>=0 and dx<m and dy>=0 and dy<n and grid[dx][dy]== "1":
-                                if (dx,dy) not in visited:
-                                    visited.add((dx,dy))
-                                    queue.append((dx,dy))
+                        for a,b in adj:
+                            dx,dy = x+a,y+b
+                            if dx>=0 and dx<m and dy>=0 and dy<n and grid[dx][dy] == "1" and (dx,dy) not in visited:
+                                visited.add((dx,dy))
+                                queue.append((dx,dy))
+
         return ret
