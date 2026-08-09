a53fdcf3c16be080071d2f6582dd70ea75f7a5c3
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-08-09 19:36:36 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (33.30%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -3,8 +3,8 @@ class Solution:
         m,n = len(str1),len(str2)
         min_r = min(m,n)
         for r in range(min_r,0,-1):
-            if m%r ==0 and n%r ==0:
+            if m %r  == 0 and  n%r  == 0:
                 curr = str1[:r]
-                if curr*(m//r) ==str1 and curr*(n//r) ==str2:
+                if curr*(m//r) == str1 and curr*(n//r) == str2:
                     return curr
         return ""
