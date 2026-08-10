ac680ae2626040dc9dfdb4b3eac4243d91ceb2ab
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-08-10 03:05:35 -0400

    Time: 0 ms (100.00%) | Memory: 55.3 MB (80.36%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.ts
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.ts
@@ -1,16 +1,13 @@
 function gcdOfStrings(str1: string, str2: string): string {
-    let m = str1.length 
-    let n = str2.length
-    let min_r =Math.min(m,n)
-    for (let r = min_r; r>0; r-- ){
-        if (m%r ===0 && n%r ===0){
-            let curr = str1.slice(0,r)
-            if (curr.repeat(Math.floor(m/r)) ==str1 && 
-            curr.repeat(Math.floor(n/r)) == str2){
-                return curr
-            }
+  const [m,n] = [str1.length, str2.length]
+  const min_r = Math.min(m,n)
+  for (let k = min_r; k>=0;k--){
+    if ( m%k ==0 && n%k == 0){
+        let curr = str1.slice(0,k)
+        if (curr.repeat(Math.floor(m/k))==str1 && curr.repeat(Math.floor(n/k))==str2) {
+            return curr
         }
-
     }
-    return ""
+  }
+  return ""
 };
