1443bd09bbe4bc2107976bce25dfd48a0d816347
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-08-24 17:04:16 -0400

    Time: 143 ms (7.81%) | Memory: 19.3 MB (88.05%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -1,27 +1,28 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         queue = list(senate)
-        r_count = d_count = r_ban = d_ban =0
-        for s in senate:
-            if s == "R":
+        r_count = d_count = d_ban = r_ban = 0
+        for i in senate:
+            if i == "R":
                 r_count+=1
             else:
-                d_count +=1
+                d_count+=1
         while queue:
             curr = queue.pop(0)
             if curr == "R":
                 if r_ban>0:
-                    r_ban-=1
+                    r_ban -=1
                 else:
                     d_ban+=1
                     queue.append(curr)
             else:
                 if d_ban>0:
-                    d_ban-=1
+                    d_ban -=1
                 else:
-                    r_ban+=1
+                    r_ban +=1
                     queue.append(curr)
-            if r_ban>=r_count:
+            if r_count<=r_ban:
                 return "Dire"
-            if d_ban>=d_count:
+            if d_count<=d_ban:
                 return "Radiant"
+
