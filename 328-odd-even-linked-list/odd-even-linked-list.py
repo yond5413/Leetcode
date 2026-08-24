75b822a1c719d4518447bc8b5ef2805516881023
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2026-08-24 17:41:07 -0400

    Time: 0 ms (100.00%) | Memory: 21.1 MB (76.34%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -8,11 +8,12 @@ class Solution:
         if not head or not head.next:
             return head
         odd = head
-        even_head = even = head.next
-        while (even and even.next):
+        even = head.next
+        even_head = even
+        while even and even.next :
             odd.next = odd.next.next
             even.next = even.next.next
             odd = odd.next
             even = even.next
         odd.next = even_head
-        return head
+        return head 
